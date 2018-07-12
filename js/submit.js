d40ff7e19b74cb85a2edfb49410079100b0fd703
Sanchez <juan.diego.c.sanchez@accenture.com>
2018-07-12 17:59:53 +0800

    Added form submission functionality

---
+++ js/submit.js
@@ -0,0 +1,25 @@
+$("form").submit(function(event){
+	event.preventDefault();
+	var serviceUrl = "http://localhost:8080/sentinel/create/carseller";
+	var formData = $(this).serializeArray();
+
+	var requestData = {};
+	formData.map(function(field){
+		requestData[field.name] = field.value;
+	});
+	requestData = JSON.stringify(requestData);
+
+	$.ajax({
+		type : "POST",
+		url : serviceUrl,
+		data : requestData,
+		dataType : "text",
+		contentType : "application/json; charset=utf-8",
+		success : function(data, status, xhr){
+			alert("Success: " + xhr.responseText);
+		},
+		error : function(xhr, status, error){
+			alert("Oops! An error occurred.");
+		}
+	});
+});
